fd73995d916d10a5171394439dde86e20a053a4b
55036662 <zhippuden@gmail.com>
2020-04-11 00:26:47 +0200

    Server settings and middlewares are configured.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-const express = require('express');
+import '@babel/polyfill';
+import 'dotenv/config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import path from 'path';
+
+///Merge TypeDefs and Resolvers from graphql folder
+const typeDefs = mergeTypes(fileLoader(path.join(__dirname, '/graphql/schemas')));
+const resolvers = mergeResolvers(fileLoader(path.join(__dirname, '/graphql/resolvers')));
 
 ///declare the app constant
 const app = express();
+
+///settings
+app.set('PORT', process.env.PORT || 4000);
+
+///Middlewares
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: ({ req }) => ({ req }),
+});
+server.applyMiddleware({ app });
 
 ///export app
 module.exports = app;
