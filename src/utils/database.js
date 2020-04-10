38108a12f09bf3266b8427a960f2cae7dc8b077b
55036662 <zhippuden@gmail.com>
2020-04-11 00:28:41 +0200

    Atlas DB conection configured and running well.

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,1 +1,22 @@
-const mongosse = require('mongoose');
+import mongoose from 'mongoose';
+
+///connection function
+export const connectDB = async () => {
+	///get the URI from dotenv file
+	const URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/CustomerManagementSystem';
+
+	///try to connect the to the Atlas DB
+	try {
+		await mongoose.connect(URI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
+		console.log(`>>>Cloud DB is connected!<<<`);
+	} catch (e) {
+		///catch if something wrong happens
+		console.error(`¡Something goes wrong!`);
+		console.error(e);
+	}
+};
