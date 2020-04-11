446735681a98324890aa51df7e59c6e2ebd5660f
55036662 <zhippuden@gmail.com>
2020-04-11 15:29:50 +0200

    Timestamps activated on models and other minor fixes.

--- src/models/Image.js
+++ src/models/Image.js
@@ -1,26 +1,32 @@
 import { Schema, model } from 'mongoose';
 
-const imageSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
+const imageSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		size: {
+			type: String,
+			required: true,
+		},
+		pacientId: {
+			type: String,
+			required: true,
+		},
+		dateId: {
+			type: String,
+			required: true,
+		},
+		url: {
+			type: String,
+			required: true,
+			trim: true,
+		},
 	},
-	size: {
-		type: String,
-		required: true,
-	},
-	pacientId: {
-		type: String,
-		required: true,
-	},
-	dateId: {
-		type: String,
-		required: true,
-	},
-	fullroute: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 export default model('Image', imageSchema);
--- src/models/User.js
+++ src/models/User.js
@@ -1,48 +1,51 @@
 import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({
-	firstname: {
-		type: String,
-		required: true,
-		trim: true,
+const userSchema = new Schema(
+	{
+		firstname: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lastname: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		profilePic: {
+			alt: { type: String },
+			url: { type: String },
+		},
+		status: {
+			type: Number,
+			default: 1,
+		},
+		range: {
+			type: Number,
+			default: 2,
+		},
+		bachTitle: {
+			type: String,
+			default: 'User',
+		},
+		userIcon: {
+			type: String,
+			default: 'fas fa-user',
+		},
 	},
-	lastname: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	profilePic: {
-		alt: { type: String },
-		url: { type: String },
-	},
-	status: {
-		type: Number,
-		default: 1,
-	},
-	range: {
-		type: Number,
-		default: 2,
-	},
-	bachtitle: {
-		type: String,
-		default: 'User',
-	},
-	usericon: {
-		type: String,
-		default: 'user',
-	},
-	createdAt: String,
-	updatedAt: String,
-});
+	{
+		timestamps: true,
+	}
+);
 
 export default model('User', userSchema);
